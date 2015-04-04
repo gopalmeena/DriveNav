7371e873e57bb51458cf9710b096709785b2eb13
gopalmeena <gopalmeena94@gmail.com>
2015-04-04 19:49:05 +0530

    check it

--- js/router.js
+++ js/router.js
@@ -1,36 +1,55 @@
-var rendererOptions = {
-  draggable: true
-};
-var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);;
-var directionsService = new google.maps.DirectionsService();
-var map;
+$(document).ready(function() {
+  $.ajax({
+    url: "data/routedata.json",
+    dataType: "json",
+    function(Data) {
+      var datalength = issueList.length;
+      var waypoints_array;
+      var placename, placelatitude,placelongitude;
 
-var warangal = new google.maps.LatLng(17.9341901, 79.81314054);
+      var WAYpoint = Data.waypoint_pos;
 
-function initialize() {
+      for(i=0; i<datalength; i++) {
+        waypoints_array = WAYpoint[i].name;
+        placelatitude = WAYpoint[i].latitude;
+        placelongitude = WAYpoint[i].longitude;
+      }
 
-  var mapOptions = {
-    zoom: 7,
-    center: warangal
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-  directionsDisplay.setMap(map);
-  directionsDisplay.setPanel(document.getElementById('directionsPanel'));
+      var rendererOptions = {
+        draggable: true
+      };
+      var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);;
+      var directionsService = new google.maps.DirectionsService();
+      var map;
 
-  google.maps.event.addListener(directionsDisplay, 'directions_changed', function() {
-    computeTotalDistance(directionsDisplay.getDirections());
-  });
-  getLocation();
+      var place = new google.maps.LatLng(placelatitude, placelongitude);
+
+      function initialize() {
+
+        var mapOptions = {
+          zoom: 7,
+          center: place
+        };
+        map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        directionsDisplay.setMap(map);
+        directionsDisplay.setPanel(document.getElementById('directionsPanel'));
+
+        google.maps.event.addListener(directionsDisplay, 'directions_changed', function() {
+          computeTotalDistance(directionsDisplay.getDirections());
+        });
+        getLocation();
   // calcRoute();
 }
 
 function calcRoute(current_pos) {
+
   var request = {
     origin: current_pos,
-    destination: 'Warangal, Telangana',
-    waypoints:[{location: 'Spencers, Warangal'}, {location: 'Municipal Corportation, Warangal'}],
+    destination: destination_pos,
+    waypoints: waypoints_array,
     travelMode: google.maps.TravelMode.DRIVING
   };
+
   directionsService.route(request, function(response, status) {
     if (status == google.maps.DirectionsStatus.OK) {
       directionsDisplay.setDirections(response);
@@ -39,15 +58,15 @@ function calcRoute(current_pos) {
 }
 
 function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-        current_pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-        calcRoute(current_pos);
-      });
-    } else { 
-        alert ("Geolocation is not supported by this browser.");
-    }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      current_pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      calcRoute(current_pos);
+    });
+  } else { 
+    alert ("Geolocation is not supported by this browser.");
   }
+}
 
 function computeTotalDistance(result) {
   var total = 0;
@@ -59,4 +78,9 @@ function computeTotalDistance(result) {
   document.getElementById('total').innerHTML = total + ' km';
 }
 
+//google.maps.event.addDomListener(window, 'load', initialize);
+
+}
+});
+})
 google.maps.event.addDomListener(window, 'load', initialize);
